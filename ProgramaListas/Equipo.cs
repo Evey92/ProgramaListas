9e023c4747bf6fd315e275d550f412bddcb74a1a
Ivan <idv16c.ipreciado@uartesdigitales.edu.mx>
2018-02-22 22:49:58 -0600

    agregué Equipo.cs

---
+++ ProgramaListas/Equipo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaListas
+{
+    class Equipo
+    {
+        string Nombre;
+        List<string> integrantes;
+
+        public void setNombre(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+        public string getNombre()
+        {
+            return this.Nombre;   
+        }
+        //idkwtimdoing
+
+
+    }
+}
